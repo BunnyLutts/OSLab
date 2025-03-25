436d6c5ba251d699aa629d4e815016577f7fdfcc
BunnyLutts <lutts@qq.com>
2025-03-25 13:46:16 +0800

    Finishing backtrace; adding backtrace to panic()

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -132,4 +133,13 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace(void) {
+    uint64 framep = r_fp();
+    uint64 page = PGROUNDDOWN(framep);
+    printf("backtrace:\n");
+    for (; page == PGROUNDDOWN(framep); framep = *(uint64 *) ((char*)(framep-16))) {
+        printf("%p\n", *(uint64 *) ((char*)framep-8));
+    }
 }
