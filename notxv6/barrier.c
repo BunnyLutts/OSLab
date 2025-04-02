6fd7f240db0b73557aaaac462dd007ee642d37f0
bunnylutts <lutts@qq.com>
2025-04-02 16:24:42 +0800

    Complete barrier.c, passed all the tests

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -22,15 +22,31 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
-static void 
-barrier()
-{
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+static void
+barrier() {
+    // YOUR CODE HERE
+    //
+    // Block until all threads have called barrier() and
+    // then increment bstate.round.
+    //
+    pthread_mutex_lock(&bstate.barrier_mutex);
+    int round = bstate.round;
+    for (;;) {
+        bstate.nthread++;
+        if (bstate.nthread == nthread) {
+            bstate.round++;
+            bstate.nthread = 0;
+            pthread_mutex_unlock(&bstate.barrier_mutex);
+            pthread_cond_broadcast(&bstate.barrier_cond);
+            return;
+        } else {
+            pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+            if (bstate.round != round) {
+                pthread_mutex_unlock(&bstate.barrier_mutex);
+                return;
+            }
+        }
+    }
 }
 
 static void *
