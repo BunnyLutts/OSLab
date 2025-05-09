2a851766d0a7a4d999a87ac023d1a71c2078becd
bunnylutts <lutts@qq.com>
2025-05-09 15:45:05 +0800

    Finish print a page table(maybe)

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint_walk(pagetable_t pagetable, int dep) {
+    for (int i=0; i<512; i++) {
+        pte_t pte = pagetable[i];
+        if (pte & PTE_V) {
+            // Print the current entry
+            for (int j=1; j<=dep; j++) {
+                printf(" ..");
+            }
+            printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+            if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+                vmprint_walk((pagetable_t)PTE2PA(pte), dep + 1);
+            }
+        }
+    }
+}
+
+void vmprint(pagetable_t pagetable) {
+    printf("page table %p\n", pagetable);
+    vmprint_walk(pagetable, 1);
+}
