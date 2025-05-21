2ff48117535aad70941f021cbcc4a128d4b3e44e
bunnylutts <lutts@qq.com>
2025-05-21 16:31:10 +0800

    Start cow, rewriting uvmcopy..

--- kernel/trap.c
+++ kernel/trap.c
@@ -50,7 +50,8 @@ usertrap(void)
   // save user program counter.
   p->trapframe->epc = r_sepc();
   
-  if(r_scause() == 8){
+  uint64 scause = r_scause();
+  if(scause == 8){
     // system call
 
     if(killed(p))
@@ -65,6 +66,12 @@ usertrap(void)
     intr_on();
 
     syscall();
+  } else if (scause == 15) {
+    // Store / AMO page fault
+    if (killed(p))
+      exit(-1);
+
+    // uint64 addr = r_stval();
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
