2863aab2449a499d46fcee4aff200eacb5662a87
bunnylutts <lutts@qq.com>
2025-06-02 22:19:26 +0800

    Fnish lab fs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -332,6 +332,30 @@ sys_open(void)
       iunlockput(ip);
       end_op();
       return -1;
+    } else if (ip->type == T_SYMLINK && omode != O_NOFOLLOW) {
+      char name[MAXPATH];
+      int dep;
+      for (dep = 10; dep > 0; dep--) {
+        if (readi(ip, 0, (uint64)name, 0, MAXPATH) < MAXPATH) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        iunlockput(ip);
+        if ((ip = namei(name)) == 0) {
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        if (ip->type != T_SYMLINK) {
+          break;
+        }
+      }
+      if (dep == 0) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
     }
   }
 
@@ -502,5 +526,30 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64 sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *dp;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  if ((dp = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+  if (writei(dp, 0, (uint64)target, 0, MAXPATH) < MAXPATH) {
+    iunlockput(dp);
+    end_op();
+    return -1;
+  }
+  iunlockput(dp);
+
+  end_op();
+
   return 0;
 }
--- user/user.h
+++ user/user.h
@@ -31,6 +31,7 @@ int trace(int);
 int sysinfo(struct sysinfo*);
 int sigalarm(int ticks, void (*handler)());
 int sigreturn(void);
+int symlink(char *target, char *path);
 #ifdef LAB_NET
 int connect(uint32, uint16, uint16);
 #endif
